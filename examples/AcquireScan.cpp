ada582d25ac29b25b4c4b65c8a21348fb4ab55a5
Papa Libasse Sow <papelibassesow@hotmail.com>
2022-06-16 13:13:38 +0200

    Removed unnecessary wait from the AcquireScan example. With future, calling get implicitly calls wait.

--- examples/AcquireScan.cpp
+++ examples/AcquireScan.cpp
@@ -70,7 +70,6 @@ int main(int argc, char** argv)
                               .withWatchdog()
                               .withWatchdogTimeout(5000)};
     auto future{Device::DataLinkBuilder(handleParameters).build(device, 1s)};
-    future.wait();
     auto [requestResult, dataLink]{future.get()};
     if (requestResult != Device::RequestResult::SUCCESS)
     {
